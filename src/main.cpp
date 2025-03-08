63b7725ce773f5f3b1d035bc169df1193cfdaaeb
maelsilvatt <ismaelsoares.pro@gmail.com>
2025-03-08 18:59:45 -0300

    Adicionadas telas ao vTaskUpdateDisplay

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <stdbool.h>
 
 // Sensores
 #include <DHTesp.h>
@@ -67,20 +68,46 @@ const unsigned long DISPLAY_INTERVAL = 2000;
 
 // Armazenamento de leituras
 typedef struct {
-  float temperatureDHT = 0;
-  float humidityDHT = 0;
-  float accelX = 0;
-  float accelY = 0;
-  float accelZ = 0;
-  float rad_level  = 0;
-  float temperatureNTC = 0;
+  char temperatureDHT[10];  
+  char humidityDHT[10];     
+  char accelX[10];          
+  char accelY[10];          
+  char accelZ[10];          
+  char rad_level[10];       
+  char temperatureNTC[10];  
 } Sensor_data;
 
 Sensor_data sensor_data;
 
-// Telas do display
+// Enum das telas do sistema
+typedef enum {
+  SCR_LOADING_1,
+  SCR_LOADING_2,
+  SCR_LOADING_3,
+  SCR_HOME,
+  SCR_WELCOME,
+  SCR_WELCOME_CREDENTIALS,
+  SCR_MENU_START,
+  SCR_MENU_VERIFY,
+  SCR_MENU_CREDITS,
+  SCR_MENU_TURN_OFF,
+  SCR_SENSORS,
+  SCR_ERROR,
+  SCR_CREDITS,
+  SCR_PROCEDURE_1,
+  SCR_PROCEDURE_2,
+  SCR_PROCEDURE_3,
+  SCR_ANALYZING,
+  SCR_RADIATION,
+  SCR_HUMIDITY,
+  SCR_TEMPERATURE,
+  SCR_GENERATING,
+  SCR_COMPLETED
+} ScreenIndex;
+
+// Telas do sistema
 typedef struct {
-  const char* screens[22][4] = {
+  char* screens[22][4] = {
     { "", "Loading system.", "", "Initializing subsystems..." }, // Iniciando sistema 1
     { "", "Loading system..", "", "Checking Vault Integrity..." }, // Iniciando sistema 2
     { "", "Loading system...", "", "Booting up protocols..." }, // Iniciando sistema 3
@@ -104,14 +131,15 @@ typedef struct {
     { "Generating plant life", "Deploying seeds...", "Monitoring growth", "" }, // Criando vegetação
     { "Terraforming Complete!", "Life support stable", "Welcome to Eden", "System shutting down..." } // Processo concluído
   };
-
-  int currentScreen = 0; // Índice da tela atual
-  bool in_menu = false; // Indica se o usuário está no menu
+  
+  ScreenIndex currentScreen; // Índice tipado
+  bool in_menu;
+  int buttonPressed = -1;
 } Display_data;
 
 Display_data display_data;
 
-
+// Event listener do sensor MPU6050
 sensors_event_t accelEvent;
 
 // Handlers do FreeRTOS
@@ -131,6 +159,7 @@ void setupSensors();
 void checkSensorConnection(bool success, const char* sensorName);
 void controlServo();
 void toggleRelay();
+void writeToLCD(const char* line1, const char* line2, const char* line3, const char* line4);
 
 void vTaskUpdateDisplay(void *pvParams);
 void vTaskReadButtons(void *pvParams);
@@ -220,8 +249,9 @@ void vTaskReadDHT(void *pvParams){
     // Protege o acesso aos dados compartilhados
     if (xSemaphoreTake(x_mutex, portMAX_DELAY)){
       // Se o status do sensor for bom (sem erro)
-      if (dhtSensor.getStatus() == 0) {        
-        sensor_data.humidityDHT = dhtData.humidity;
+      if (dhtSensor.getStatus() == 0) {                
+        // Armazena o dado do sensor em char[]
+        dtostrf(dhtData.humidity, 4, 0, sensor_data.humidityDHT);
       }
       
       // Liberar o mutex após a leitura
@@ -238,6 +268,7 @@ void vTaskReadRadScan3000(void *pvParams) {
     if (xSemaphoreTake(x_mutex, portMAX_DELAY)) {
       // Leitura da temperatura do BMP280 e interpreta como radiação
       float temp = bmp.readTemperature();
+      float tempMappedRad;
 
       // Definição dos limites
       float tempMin = -123.5;
@@ -246,12 +277,15 @@ void vTaskReadRadScan3000(void *pvParams) {
       float radMax = 99.0;
 
       // Mapeamento linear da temperatura para radiação
-      sensor_data.rad_level = (temp - tempMin) * (radMax - radMin) / (tempMax - tempMin) + radMin;
+      tempMappedRad = (temp - tempMin) * (radMax - radMin) / (tempMax - tempMin) + radMin;
 
       // Garantir que o valor de radiação fique dentro dos limites 0-99
-      if (sensor_data.rad_level < radMin) sensor_data.rad_level = radMin;
-      if (sensor_data.rad_level > radMax) sensor_data.rad_level = radMax;
+      if (tempMappedRad < radMin) tempMappedRad = radMin;
+      if (tempMappedRad > radMax) tempMappedRad = radMax;
       
+      // Armazena o dado do sensor em char[]
+      dtostrf(tempMappedRad, 4, 0, sensor_data.rad_level);
+
       // Libera o mutex
       xSemaphoreGive(x_mutex);
     }
@@ -268,10 +302,10 @@ void vTaskReadMPU(void *pvParams) {
       // Leitura do MPU6050 (acelerômetro)
       mpu.getAccelerometerSensor()->getEvent(&accelEvent);
 
-      // Armazenar os dados do acelerômetro
-      sensor_data.accelX = accelEvent.acceleration.x;
-      sensor_data.accelY = accelEvent.acceleration.y;
-      sensor_data.accelZ = accelEvent.acceleration.z;
+      // Armazena o dado do sensor em char[]
+      dtostrf(accelEvent.acceleration.x, 4, 0, sensor_data.accelX);
+      dtostrf(accelEvent.acceleration.y, 4, 0, sensor_data.accelY);
+      dtostrf(accelEvent.acceleration.z, 4, 0, sensor_data.accelZ);
       
       // Liberar o mutex após a leitura
       xSemaphoreGive(x_mutex);
@@ -287,11 +321,15 @@ void vTaskReadNTC(void *pvParams) {
     if (xSemaphoreTake(x_mutex, portMAX_DELAY)) {
       // Leitura do ADC
       int analogValue = analogRead(NTC_PIN);
+      float converted_reading;
 
       // Converte para Celsius
       if (analogValue > 0 && analogValue < 4095) {                         
-        sensor_data.temperatureNTC = 1 / (log(1 / (4095. / analogValue - 1)) / BETA + 1.0 / 298.15) - 273.15;        
+        converted_reading = 1 / (log(1 / (4095. / analogValue - 1)) / BETA + 1.0 / 298.15) - 273.15;        
       }
+
+      // Armazena o dado do sensor em char[]
+      dtostrf(converted_reading, 4, 0, sensor_data.temperatureNTC);
 
       xSemaphoreGive(x_mutex);
     }
@@ -300,37 +338,108 @@ void vTaskReadNTC(void *pvParams) {
   }
 }
 
+// Função auxiliar para escrever nas linhas do display LCD
+void writeToLCD(const char* line1, const char* line2, const char* line3, const char* line4) {
+  // Limpar o display
+  lcd.clear();
+
+
+  lcd.setCursor(0, 0);  // Coloca o cursor na primeira linha, coluna 0
+  lcd.print(line1);     
+
+  lcd.setCursor(0, 1);  // Coloca o cursor na segunda linha, coluna 0
+  lcd.print(line2);      
+
+  lcd.setCursor(0, 2);  // Coloca o cursor na terceira linha, coluna 0
+  lcd.print(line3);   
+
+  lcd.setCursor(0, 3);  // Coloca o cursor na quarta linha, coluna 0
+  lcd.print(line4);     
+}
+
 // Task de atualização do display
 void vTaskUpdateDisplay(void *pvParams) {
   while (true) {
     // Protege o acesso aos dados compartilhados
     if (xSemaphoreTake(x_mutex, portMAX_DELAY)) {
-      // Limpar o display
-      lcd.clear();
+      char RAD_INFO[16]; 
+      char TEMP_INFO[16]; 
+      char HUMIDITY_INFO[16]; 
       
-      // Exibir umidade
-      lcd.setCursor(0, 0);
-      lcd.print("Humidity: ");
-      lcd.print(sensor_data.humidityDHT, 1);
-      lcd.print(" %");
+      sprintf(RAD_INFO, "Rad: %s microSv/h", sensor_data.rad_level);
+      sprintf(TEMP_INFO, "Temperature: %s  C", sensor_data.temperatureNTC);
+      sprintf(HUMIDITY_INFO, "Humidity: %s  %", sensor_data.humidityDHT);
 
-      // Exibir aceleração
-      lcd.setCursor(0, 1);
-      lcd.print("Acell Y: ");
-      lcd.print(sensor_data.accelY, 1);
-      lcd.print(" m/s²");
-
-      // Exibir radiação
-      lcd.setCursor(0, 2);
-      lcd.print("Radiation: ");
-      lcd.print(sensor_data.rad_level , 1);
-      lcd.print(" Sv/h");
-
-      // Exibir temperatura NTC
-      lcd.setCursor(0, 3);
-      lcd.print("Temperature: ");
-      lcd.print(sensor_data.temperatureNTC, 1);
-      lcd.print(" C");    
+      switch (display_data.currentScreen) {
+        case SCR_LOADING_1:
+            writeToLCD(display_data.screens[SCR_LOADING_1][0], display_data.screens[SCR_LOADING_1][1], display_data.screens[SCR_LOADING_1][2], display_data.screens[SCR_LOADING_1][3]);
+            break;
+        case SCR_LOADING_2:
+            writeToLCD(display_data.screens[SCR_LOADING_2][0], display_data.screens[SCR_LOADING_2][1], display_data.screens[SCR_LOADING_2][2], display_data.screens[SCR_LOADING_2][3]);
+            break;
+        case SCR_LOADING_3:
+            writeToLCD(display_data.screens[SCR_LOADING_3][0], display_data.screens[SCR_LOADING_3][1], display_data.screens[SCR_LOADING_3][2], display_data.screens[SCR_LOADING_3][3]);
+            break;
+        case SCR_HOME:
+            writeToLCD(display_data.screens[SCR_HOME][0], display_data.screens[SCR_HOME][1], display_data.screens[SCR_HOME][2], display_data.screens[SCR_HOME][3]);
+            break;
+        case SCR_WELCOME:
+            writeToLCD(display_data.screens[SCR_WELCOME][0], display_data.screens[SCR_WELCOME][1], display_data.screens[SCR_WELCOME][2], display_data.screens[SCR_WELCOME][3]);
+            break;
+        case SCR_WELCOME_CREDENTIALS:
+            writeToLCD(display_data.screens[SCR_WELCOME_CREDENTIALS][0], display_data.screens[SCR_WELCOME_CREDENTIALS][1], display_data.screens[SCR_WELCOME_CREDENTIALS][2], display_data.screens[SCR_WELCOME_CREDENTIALS][3]);
+            break;
+        case SCR_MENU_START:
+            writeToLCD(display_data.screens[SCR_MENU_START][0], display_data.screens[SCR_MENU_START][1], display_data.screens[SCR_MENU_START][2], display_data.screens[SCR_MENU_START][3]);
+            break;
+        case SCR_MENU_VERIFY:
+            writeToLCD(display_data.screens[SCR_MENU_VERIFY][0], display_data.screens[SCR_MENU_VERIFY][1], display_data.screens[SCR_MENU_VERIFY][2], display_data.screens[SCR_MENU_VERIFY][3]);
+            break;
+        case SCR_MENU_CREDITS:
+            writeToLCD(display_data.screens[SCR_MENU_CREDITS][0], display_data.screens[SCR_MENU_CREDITS][1], display_data.screens[SCR_MENU_CREDITS][2], display_data.screens[SCR_MENU_CREDITS][3]);
+            break;
+        case SCR_MENU_TURN_OFF:
+            writeToLCD(display_data.screens[SCR_MENU_TURN_OFF][0], display_data.screens[SCR_MENU_TURN_OFF][1], display_data.screens[SCR_MENU_TURN_OFF][2], display_data.screens[SCR_MENU_TURN_OFF][3]);
+            break;
+        case SCR_SENSORS:
+            writeToLCD(RAD_INFO, TEMP_INFO, HUMIDITY_INFO, display_data.screens[SCR_SENSORS][3]);
+        case SCR_ERROR:
+            writeToLCD(display_data.screens[SCR_ERROR][0], display_data.screens[SCR_ERROR][1], display_data.screens[SCR_ERROR][2], display_data.screens[SCR_ERROR][3]);
+            break;
+        case SCR_CREDITS:
+            writeToLCD(display_data.screens[SCR_CREDITS][0], display_data.screens[SCR_CREDITS][1], display_data.screens[SCR_CREDITS][2], display_data.screens[SCR_CREDITS][3]);
+            break;
+        case SCR_PROCEDURE_1:
+            writeToLCD(display_data.screens[SCR_PROCEDURE_1][0], display_data.screens[SCR_PROCEDURE_1][1], display_data.screens[SCR_PROCEDURE_1][2], display_data.screens[SCR_PROCEDURE_1][3]);
+            break;
+        case SCR_PROCEDURE_2:
+            writeToLCD(display_data.screens[SCR_PROCEDURE_2][0], display_data.screens[SCR_PROCEDURE_2][1], display_data.screens[SCR_PROCEDURE_2][2], display_data.screens[SCR_PROCEDURE_2][3]);
+            break;
+        case SCR_PROCEDURE_3:
+            writeToLCD(display_data.screens[SCR_PROCEDURE_3][0], display_data.screens[SCR_PROCEDURE_3][1], display_data.screens[SCR_PROCEDURE_3][2], display_data.screens[SCR_PROCEDURE_3][3]);
+            break;
+        case SCR_ANALYZING:
+            writeToLCD(display_data.screens[SCR_ANALYZING][0], display_data.screens[SCR_ANALYZING][1], display_data.screens[SCR_ANALYZING][2], display_data.screens[SCR_ANALYZING][3]);
+            break;
+        case SCR_RADIATION:
+            writeToLCD(display_data.screens[SCR_RADIATION][0], display_data.screens[SCR_RADIATION][1], display_data.screens[SCR_RADIATION][2], RAD_INFO);
+            break;
+        case SCR_HUMIDITY:
+            writeToLCD(display_data.screens[SCR_HUMIDITY][0],display_data.screens[SCR_HUMIDITY][1], display_data.screens[SCR_HUMIDITY][2], HUMIDITY_INFO);
+            break;
+        case SCR_TEMPERATURE:
+            writeToLCD(display_data.screens[SCR_TEMPERATURE][0], display_data.screens[SCR_TEMPERATURE][1], display_data.screens[SCR_TEMPERATURE][2], TEMP_INFO);
+            break;
+        case SCR_GENERATING:
+            writeToLCD(display_data.screens[SCR_GENERATING][0],display_data.screens[SCR_GENERATING][1],display_data.screens[SCR_GENERATING][2], display_data.screens[SCR_GENERATING][3]);
+            break;
+        case SCR_COMPLETED:
+            writeToLCD(display_data.screens[SCR_COMPLETED][0], display_data.screens[SCR_COMPLETED][1], display_data.screens[SCR_COMPLETED][2], display_data.screens[SCR_COMPLETED][3]);
+            break;
+        default:
+            // Handle default case
+            break;
+    }    
 
       // Liberar o mutex após a leitura
       xSemaphoreGive(x_mutex);
@@ -356,13 +465,13 @@ void vTaskReadButtons(void *pvParams) {
     if (xSemaphoreTake(x_mutex, portMAX_DELAY)) {
       // Leitura dos botões
       if (digitalRead(UP_PIN) == LOW) {
-        sensor_data.buttonPressed = 0;
+        display_data.buttonPressed = 0;
       } else if (digitalRead(DOWN_PIN) == LOW) {
-        sensor_data.buttonPressed = 1;
+        display_data.buttonPressed = 1;
       } else if (digitalRead(SELECT_PIN) == LOW) {
-        sensor_data.buttonPressed = 2;
+        display_data.buttonPressed = 2;
       } else {
-        sensor_data.buttonPressed = -1;
+        display_data.buttonPressed = -1;
       }
       
       xSemaphoreGive(x_mutex);
